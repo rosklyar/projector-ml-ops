8d86aeb834aba11b46829359c3f54cb97ba80070
rskliar <rskliar@playtika.com>
2023-05-08 11:45:03 +0300

    add experiments training routine using W&B + model card generation

--- garbage-classifier/garbage-classifier/garbage_data.py
+++ garbage-classifier/garbage-classifier/garbage_data.py
@@ -31,3 +31,25 @@ class LazyDataset(Dataset):
     
     def __len__(self):
         return len(self.dataset)
+    
+class GarbageData():
+
+    def __init__(self, dataset_path, batch_size, val_split=0.2):
+        self.dataset_path = dataset_path
+        self.batch_size = batch_size
+        self.feature_extractor = FeatureExtractor('microsoft/beit-base-patch16-224-pt22k-ft22k')
+        data_transform_train = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
+            transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15),
+            self.feature_extractor])
+        self.data = ImageFolder(self.dataset_path)
+        train_data, validation_data = random_split(self.data, [int(len(self.data) * (1 - val_split)), int(len(self.data) * val_split)])
+        self._train_loader = DataLoader(LazyDataset(train_data, data_transform_train), batch_size=self.batch_size, shuffle=True, num_workers=4)
+        self._val_loader = DataLoader(LazyDataset(validation_data, transforms.Compose([self.feature_extractor])), batch_size=self.batch_size, shuffle=False, num_workers=4)
+
+    def get_train_loader(self):
+        return self._train_loader
+
+    def get_val_loader(self):
+        return self._val_loader
---
+++ garbage-classifier/garbage-classifier/model_card.py
@@ -0,0 +1,48 @@
+import torch
+import psutil
+
+
+def create_model_card(model_name, model_description, data_details, preprocessing_details, training_details, evaluation_details, usage, limitations):
+    model_card = f"""# {model_name}
+
+## Model Description
+
+{model_description}
+
+## Dataset
+
+{data_details}
+
+## Training procedure
+
+### Preprocessing
+{preprocessing_details}
+
+### Training
+{training_details}
+
+#### Training System Hardware
+- GPU: {torch.cuda.device_count()}
+- GPU type: {torch.cuda.get_device_name(0)}
+- CPU: {psutil.cpu_count()}
+
+## Evaluation
+
+{evaluation_details}
+
+## Usage
+
+{usage}
+
+## Limitations
+
+{limitations}
+"""
+
+    return model_card
+
+
+def save_model_card(filename, model_card_content):
+    with open(filename, 'w') as f:
+        f.write(model_card_content)
+        print(f"Model Card saved as {filename}")
