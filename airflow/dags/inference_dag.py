9822a69534ca02fd1b71ff08eb335a10e937b313
rskliar <rskliar@playtika.com>
2023-05-24 20:44:12 +0300

    format dags with black

--- airflow/dags/inference_dag.py
+++ airflow/dags/inference_dag.py
@@ -1,18 +1,36 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
 from kubernetes.client import models as k8s
 from airflow.models import Variable
 from airflow.models.param import Param
 
-volume = k8s.V1Volume(name="inference-storage",
-                      persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="inference-storage"),)
+volume = k8s.V1Volume(
+    name="inference-storage",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name="inference-storage"
+    ),
+)
 volume_mount = k8s.V1VolumeMount(
-    name="inference-storage", mount_path="/var/tmp/", sub_path=None)
+    name="inference-storage", mount_path="/var/tmp/", sub_path=None
+)
 IMAGE = "rostyslavskliar/garbage-classifier-trainer:latest"
 
-with DAG(start_date=datetime(2023, 1, 1), params={"bucket": Param("nowastecomua", type="string"), "folder": Param("test", type="string"),  "model_name": Param("garbage-classifier-model", type="string"),  "version": Param("v0", type="string")}, catchup=False, schedule_interval=None, dag_id="training_dag") as dag:
+with DAG(
+    start_date=datetime(2023, 1, 1),
+    params={
+        "bucket": Param("nowastecomua", type="string"),
+        "folder": Param("test", type="string"),
+        "model_name": Param("garbage-classifier-model", type="string"),
+        "version": Param("v0", type="string"),
+    },
+    catchup=False,
+    schedule_interval=None,
+    dag_id="training_dag",
+) as dag:
     s3_access_key = Variable.get("s3_access_key")
     s3_secret_key = Variable.get("s3_secret_key")
     wandb_api_key = Variable.get("wandb_api_key")
@@ -31,23 +49,36 @@ with DAG(start_date=datetime(2023, 1, 1), params={"bucket": Param("nowastecomua"
     load_data = KubernetesPodOperator(
         name="load_data",
         image=IMAGE,
-        cmds=["python", "garbage_classifier/cli.py", "load-data",
-                 s3_access_key, s3_secret_key, dag.params["bucket"], dag.params["folder"], "/var/tmp/data/"],
+        cmds=[
+            "python",
+            "garbage_classifier/cli.py",
+            "load-data",
+            s3_access_key,
+            s3_secret_key,
+            dag.params["bucket"],
+            dag.params["folder"],
+            "/var/tmp/data/",
+        ],
         task_id="load_data",
         in_cluster=False,
         namespace="default",
         volumes=[volume],
         volume_mounts=[volume_mount],
     )
-    
+
     model_name = dag.params["model_name"]
     model_version = dag.params["version"]
 
     download_model = KubernetesPodOperator(
         name="download_model",
         image=IMAGE,
-        cmds=["python", "garbage_classifier/cli.py",
-                 "download-from-registry", model_name, model_version],
+        cmds=[
+            "python",
+            "garbage_classifier/cli.py",
+            "download-from-registry",
+            model_name,
+            model_version,
+        ],
         task_id="download_model",
         in_cluster=False,
         namespace="default",
@@ -58,8 +89,13 @@ with DAG(start_date=datetime(2023, 1, 1), params={"bucket": Param("nowastecomua"
     inference = KubernetesPodOperator(
         name="inference",
         image=IMAGE,
-        cmds=["python", "garbage_classifier/cli.py",
-                 "make-inference", f"/artifacts/{model_name}-{model_version}/model.pth", "/var/tmp/data/data.tar.gz"],
+        cmds=[
+            "python",
+            "garbage_classifier/cli.py",
+            "make-inference",
+            f"/artifacts/{model_name}-{model_version}/model.pth",
+            "/var/tmp/data/data.tar.gz",
+        ],
         task_id="inference",
         in_cluster=False,
         namespace="default",
